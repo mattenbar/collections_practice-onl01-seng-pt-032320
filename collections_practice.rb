a68789e42f0cc8bc26993229d704b6867e17c8f5
matthew enbar <matthew.enbar@gmail.com>
2020-03-27 00:25:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -12,3 +12,10 @@ def sort_array_char_count(array)
   a.length <=> b.length
   end
 end
+
+def swap_elements(array)
+  temp = array[1]
+  array [1] = array [2]
+  array [2] = temp
+  return array
+end
